6eb5c9d93e1cc7a8cdfc22f79005beb5bab7759f
Victor Bañon <81150790+VictorBanon@users.noreply.github.com>
2024-12-20 18:10:22 +0100

    templqte of RBCD(third paper algo)

---
+++ RBCD.py
@@ -0,0 +1,9 @@
+# (i)  Produce the constrained sequence graph G = GrC(S, k, M).
+
+# (ii) Uniformly generate a random Eulerian trail in G, and take its trace T .
+
+# (iii) If T has no perfect configuration then goto (ii).
+
+#( iv) Compute the number N of Eulerian trails corresponding to this particular trace T .
+
+# (v) Return T with probability 1/N , or goto (ii).
