7ab29e460edaad1d244511350229b72b5b37b7fb
daxida <galmaw@hotmail.com>
2025-01-16 14:50:28 +0100

    Add test for altschul permutation
    * Verify that ALL possible permutations are found within 1000 iterations

--- py/test_altschul.py
+++ py/test_altschul.py
@@ -1,5 +1,5 @@
 from constants import S1, S2, S3, S4
-from altschul import edge_ordering
+from altschul import edge_ordering, doublet_preserving_permutation
 from utils import same_klets, same_klons
 
 
@@ -11,3 +11,38 @@ def test_altschul_sequences():
 
 def test_edge_ordering():
     assert len(edge_ordering(S1, 3)) == 16
+
+
+def brute_force_possible_permutations(seq: str):
+    """Get naively all the permutations with same 2-lets."""
+    import itertools
+
+    all_doublet_perserving_permutations = set()
+    for perm in itertools.permutations(seq):
+        perm = "".join(perm)
+        if same_klets(perm, seq, 2):
+            all_doublet_perserving_permutations.add(perm)
+    print(list(all_doublet_perserving_permutations))
+
+
+def test_altschul_doublet_preserving_permutation():
+    """Test that all of possible 2-let preserving permutations are found."""
+    seq = "ACTAGTAT"
+
+    # Set of all possible permutations found with:
+    # brute_force_possible_permutations(seq)
+    all_perms = {
+        "AGTACTAT",
+        "ACTAGTAT",
+        "ATAGTACT",
+        "ATACTAGT",
+        "AGTATACT",
+        "ACTATAGT",
+    }
+
+    for _ in range(1000):
+        res = doublet_preserving_permutation(seq, k=2)
+        all_perms.discard(res)
+        assert same_klets(seq, res, 2)
+
+    assert len(all_perms) == 0
